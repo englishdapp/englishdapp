0b51fc43073ee917226260c159769c187ffaf367
englishdapp <englishdapp@gmail.com>
2019-06-04 01:50:09 +0900

    Updates

---
+++ precache-manifest.a5089f5aaa0a8b6e7fc15bed16f350a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03c9b8e45feb412110d6dc99a09756d",
+    "url": "/englishdapp/index.html"
+  },
+  {
+    "revision": "72263d56ed7f7f823b56",
+    "url": "/englishdapp/static/css/2.c17ff19c.chunk.css"
+  },
+  {
+    "revision": "8ae5753584302567967b",
+    "url": "/englishdapp/static/css/main.bfd32dbe.chunk.css"
+  },
+  {
+    "revision": "72263d56ed7f7f823b56",
+    "url": "/englishdapp/static/js/2.c0829146.chunk.js"
+  },
+  {
+    "revision": "8ae5753584302567967b",
+    "url": "/englishdapp/static/js/main.6953d6bf.chunk.js"
+  },
+  {
+    "revision": "d718c11382ec8a8628fe",
+    "url": "/englishdapp/static/js/runtime~main.456427fc.js"
+  },
+  {
+    "revision": "31b53e50b049e49d014034150430900f",
+    "url": "/englishdapp/static/media/ac-3.31b53e50.png"
+  },
+  {
+    "revision": "2cfb451092d0aec457af60bbe0c59bf2",
+    "url": "/englishdapp/static/media/ac-6.2cfb4510.jpg"
+  }
+]);
